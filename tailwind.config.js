a56233367c50f44f66c92453ca501ace42d8dc19
Misbau-4 <abduldamilare14@gmail.com>
2024-10-24 16:48:36 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html, js}"],
+  theme: {
+    color: {
+
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+
+      },
+      borderRadius: {
+  
+      },
+    },
+  },
+  plugins: [],
+};
+
