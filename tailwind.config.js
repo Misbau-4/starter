08542ce2a5b4bef50c042e6d3da1fc2132e8fd60
Misbau-4 <abduldamilare14@gmail.com>
2024-10-31 18:32:32 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./public/**/*.{html, js}"],
   theme: {
-    color: {
-
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
     extend: {
-      spacing: {
-
+      spacing: {},
+      borderRadius: {},
+      fontFamily: {
+        sans: ["Work Sans", "sans-serif"],
+        serif: ["Merriweather", "serif"],
       },
-      borderRadius: {
-  
+      colors: {
+        // BODY COLORS
+        "light-bg-clr": "#FEFEFE",
+        "dark-text-clr": "#09080A",
+        // CARD COLORS
+        "card-bg-clr": "#F7F6F8",
+        // PRIMARY COLORS
+        "primary-30": "#9AD0FF",
+        "primary-50": "#BDE0FF",
+        "primary-70": "#CBE7FF",
+        // SECONDARY COLORS
+        "secondary-30": "#9EDCAC",
+        "secondary-50": "#D2F1D9",
       },
     },
   },
